937f4eecc75c901395f49abd0c364c6422804d6c
Ubatham <utkarshkashyap13@gmail.com>
2025-07-21 10:18:00 +0530

    updated code

--- script.js
+++ script.js
@@ -1,322 +1,218 @@
-//console.log("connected")
+// DOM elements
+let searchWordInput = document.getElementById('search_word');
+let cardTitle = document.getElementById('card_title');
+let wordMeaning = document.getElementById('word_meaning');
+let wordExample = document.getElementById('word_example');
+let wordPronunciation = document.getElementById('word_pronounciation');
+let wordAudioSource = document.getElementById('word_audio_source');
+let wordAudio = document.getElementById('word_audio');
+let audioBtn = document.getElementById('audio_btn');
+let searchBtn = document.getElementById('search_btn');
+let resetBtn = document.getElementById('reset_btn');
+let bookmarkIcon = document.getElementById('bookmark_icon');
+let wordResultCard = document.getElementById('show_result');
+let recentSearchSection = document.getElementById("word_history_section");
+let recentTitle = document.getElementById("recent_search_title");
 
-let loading_animation = document.getElementById('loading_animation');
-/*
-$(window).load(function () {
-    // Animate loader off screen
-    $("#loading_animation").fadeOut("slow");;
-});*/
+// Hide loader initially
 $(window).on('load', function () {
     $('#loading_animation').hide();
-})
-displayRecentWord();
-displaybookmarkedWord();
-displayquote();
-/**********FETCH API************/
+    displayRecentWords();
+    displayBookmarks();
+    displayQuote();
+});
 
-// API for QUOTE
-let quote = document.getElementById('quote');
-let author = document.getElementById('author');
-async function displayquote() {
-    let quote_num = Math.floor(Math.random() * 1643)
-    const response = await fetch("https://type.fit/api/quotes")
-    const data = await response.json();
-    if (response.ok) {
-        // Update DOM elements
-        quote.innerHTML = `"${data[quote_num].text}"`
-        author.innerHTML = `~${data[quote_num].author} `
-    }
-    else {
-        fetch("http://api.quotable.io/random")
-            .then(res => res.json())// to convert into json format
-            .then(data => {
-                quote.innerHTML = `"${data.content}"`
-                author.innerHTML = `~${data.author} `
-            })
-
+// Fetch quote
+async function displayQuote() {
+    try {
+        let res = await fetch("https://type.fit/api/quotes");
+        let data = await res.json();
+        let random = Math.floor(Math.random() * data.length);
+        document.getElementById("quote").innerText = `"${data[random].text}"`;
+        document.getElementById("author").innerText = `~${data[random].author || "Anonymous"}`;
+    } catch {
+        document.getElementById("quote").innerText = `"Keep learning!"`;
+        document.getElementById("author").innerText = `~AI`;
     }
 }
 
-/**RESIZING INPUT TEXTAREA */
-
-/*
-var input = document.querySelector('input'); // get the input element
-input.addEventListener('input', resizeInput); // bind the "resizeInput" callback on "input" event
-resizeInput.call(input); // immediately call the function
-
-function resizeInput() {
-    this.style.width = this.value.length + "30ch";
-}*/
-/********** *****/
-let search_word = document.getElementById('search_word');
-let card_title = document.getElementById('card_title');
-let word_meaning = document.getElementById('word_meaning');
-let word_example = document.getElementById('word_example');
-let word_pronounciation = document.getElementById('word_pronounciation');
-let word_audio_source = document.getElementById('word_audio_source');
-let word_audio = document.getElementById('word_audio');
-let audio_btn = document.getElementById('audio_btn');
-let search_btn = document.getElementById('search_btn');
-let bookmark_btn = document.getElementById('bookmark_btn');
-let offcanvasRight = document.getElementById('offcanvasRight');
-let word_history_section = document.getElementById('word_history_section');
-
-let word_result_div = document.getElementById('word_result_div');
-
-search_btn.addEventListener('click', async function () {
-    bookmark_icon.getElementsByTagName("i")[0].className = "bi bi-bookmark";
-    let show_result = document.getElementById('show_result');
-    show_result.style.display = "none";
-    if (search_word.value == "") {
-        show_result.style.display = "none";
-        var toastLiveExample = document.getElementById('liveToast')
-        var toast = new bootstrap.Toast(toastLiveExample);
-        toast.show()
-    }
-    else {
-        fetchWord();
-        word_history_section.style.display = "none";
-        loading_animation.style.display = "block";
-
-        setTimeout(function () {
-            loading_animation.style.display = "none";
-            word_history_section.style.display = "block";
-        }, 0990)
-        setTimeout(function () {
-            show_result.style.display = "block";
-
-        }, 1000)
-
-
-    }/***local storage for recently searched  words  */
-    let recently_searched = localStorage.getItem("recently_searched");
-    if (recently_searched == null) {
-        recentWord = [];
-    } else {
-        recentWord = JSON.parse(recently_searched);
-
-    }
-    const response = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${search_word.value}`)
-    const data = await response.json();
-
-    let recentObj = {
-        title: `${data[0].word}`,
-        meaning: `${data[0].meanings[0].definitions[0].definition}`,
+// Search button click
+searchBtn.addEventListener('click', async () => {
+    let word = searchWordInput.value.trim();
+    if (!word) {
+        new bootstrap.Toast(document.getElementById('liveToast')).show();
+        return;
     }
 
-    if (search_word.value != "") {
-        recentWord.push(recentObj);
-        localStorage.setItem("recently_searched", JSON.stringify(recentWord));
-        displayRecentWord();
+    $('#loading_animation').show();
+    wordResultCard.style.display = "none";
+
+    try {
+        await fetchWord(word);
+        saveToRecent(word);
+    } finally {
+        $('#loading_animation').hide();
+        wordResultCard.style.display = "block";
+        recentSearchSection.style.display = "block";
     }
+});
 
-})
-reset_btn.addEventListener('click', function () {
-    search_word.value = "";
-    show_result.style.display = "none";
-})
-// API for WORD
-async function fetchWord() {
-    //console.log('call received for fetchWord function')
-    const response = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${search_word.value}`)
-    //console.log(`https://api.dictionaryapi.dev/api/v2/entries/en/${search_word.value}`)
-    const data = await response.json();
-    if (response.ok) {
-        audio_btn.style.display = "block";
-        bookmark_icon.style.display = "block";
+// Reset button
+resetBtn.addEventListener('click', () => {
+    searchWordInput.value = "";
+    wordResultCard.style.display = "none";
+});
 
-        card_title.innerHTML = `${data[0].word}`
-        word_meaning.innerHTML = `<br>  <b><i>Meaning: </i></b>  ${data[0].meanings[0].definitions[0].definition} `
-        word_example.innerHTML = `<b><i>Example: </i></b>   ${data[0].meanings[0].definitions[0].example} `
-        word_audio_source.src = `${data[0].phonetics[0].audio}`
-        word_pronounciation.innerHTML = `<b><i>Pronounciation:</i></b>  ${data[0].phonetics[0].text}  `
+// Fetch dictionary info
+async function fetchWord(word) {
+    try {
+        const res = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`);
+        const data = await res.json();
 
-    }
-    else {
-        card_title.innerHTML = `${data.title}`
-        word_meaning.innerHTML = `${data.message}`
-        word_example.innerHTML = `${data.resolution}`
-        word_pronounciation.innerHTML = "";
-        audio_btn.style.display = "none";
-        bookmark_icon.style.display = "none";
+        if (!res.ok) throw data;
 
-    }
-}
-//word_audio
-audio_btn.addEventListener('click', function (e) {
-    e.preventDefault();
-    audio_btn.getElementsByTagName("i")[0].className = "bi bi-volume-down-fill";
-    word_audio.load(); //call this to just preload the audio without playing
-    word_audio.play();
-    word_audio.addEventListener('ended', function () {
+        const { word: w, meanings, phonetics, origin } = data[0];
+        cardTitle.innerText = w;
 
-        audio_btn.getElementsByTagName("i")[0].className = "bi bi-volume-down";
-        console.log(audio_btn.getElementsByTagName("i")[0].className)
+        // Pronunciation
+        const phoneticText = phonetics.find(p => p.text)?.text || "N/A";
+        const audioURL = phonetics.find(p => p.audio)?.audio || "";
+        wordPronunciation.innerHTML = `<b><i>Pronunciation:</i></b> ${phoneticText}`;
+        wordAudioSource.src = audioURL;
+        audioBtn.style.display = audioURL ? "inline-block" : "none";
 
+        // Origin
+        let originHTML = origin ? `<b><i>Origin:</i></b> ${origin}<br><br>` : "";
 
-    })
-
-
-})
-//Recently searched
-function displayRecentWord() {
-    // console.log('displayRecentWord() called ');
-    let recently_searched = localStorage.getItem("recently_searched");
-    if (recently_searched == null) {
-        // console.log("recentWord was null")
-        recentWord = [];
-    } else {
-        recentWord = JSON.parse(recently_searched);
-        // console.log("recentWord was not null")
-    }
-    let html = "";
-    recentWord.forEach(function (element, index) {
-        // console.log("entered recentWord loop  fnc")
-        html += `<div class="recent_word my-2 mx-2 card" style="width: 20rem;">
-        <div class="card-body">
-           <button id ="recent_popover" type="button" class="btn " data-bs-toggle="popover" data-bs-trigger="hover focus" data-bs-placement="bottom" title="${element.title}"
-            data-bs-content="${element.meaning}"> <h5 class="card-title">${element.title}</h5>
-        </button>
+        // Meanings
+        let meaningHTML = meanings.map(m => `
+            <div>
+                <b>Part of Speech:</b> ${m.partOfSpeech}<br>
+                <b>Definitions:</b>
+                <ol>
+                    ${m.definitions.map(d => `
+                        <li>
+                            ${d.definition}
+                            ${d.example ? `<br><b>Example:</b> "${d.example}"` : ""}
+                            ${d.synonyms?.length ? `<br><b>Synonyms:</b> ${d.synonyms.join(", ")}` : ""}
+                            ${d.antonyms?.length ? `<br><b>Antonyms:</b> ${d.antonyms.join(", ")}` : ""}
+                        </li>
+                    `).join('')}
+                </ol><br>
             </div>
-    </div>`;
-        deleteRecentSearch(index);
-        //console.log(html);
-    });
-    let recently_searched_element = document.getElementById('recently_searched')
-    let recent_search_title = document.getElementById("recent_search_title");
-    if (recentWord.length != 0) {
-        recent_search_title.style.display = "block"
-        recently_searched_element.innerHTML = html;
-    }
+        `).join('');
 
-}
-function deleteRecentSearch(index) {
-    //console.log(`index ${index} given`)
-    let recently_searched = localStorage.getItem("recently_searched");
-    if (recently_searched == null) {
+        wordMeaning.innerHTML = originHTML + meaningHTML;
+        wordExample.innerHTML = "";
+        bookmarkIcon.style.display = "inline-block";
+        bookmarkIcon.querySelector("i").className = "bi bi-bookmark";
 
-        recentWord = [];
-    } else {
-        recentWord = JSON.parse(recently_searched);
-    }
-    if (index == 4) {
-        //console.log(`index ${index} therefore entered splice function part;expected index =4`)
-        recentWord.splice(0, 1);
-        //index = 0;
-        localStorage.setItem("recently_searched", JSON.stringify(recentWord));
-        displayRecentWord();
-
-    }
-    // console.log(`index:${index} before leaving  delete fnc`)
-}
-//bookmark
-let bookmark_icon = document.getElementById('bookmark_icon')
-bookmark_icon.addEventListener('click', function () {
-
-    let bookmark_icon_class = bookmark_icon.getElementsByTagName("i")[0].className
-    /***local storage for bookmarked words  */
-    let bookmarked_words = localStorage.getItem("bookmarked_words");
-    if (bookmarked_words == null) {
-        bookmarkedObj = [];
-    } else {
-        bookmarkedObj = JSON.parse(bookmarked_words);
-
-    }
-    if (bookmark_icon_class == "bi bi-bookmark") {
-
-        bookmark_icon.getElementsByTagName("i")[0].className = "bi bi-bookmark-check-fill";
-        // to be reviewed such that if class name == icon fil then
-        let bookmark_parent = this.parentElement;
-
-        bookmarkedObj.push(bookmark_parent.getElementsByTagName("h5")[0].innerHTML);// to be changed to 
-        localStorage.setItem("bookmarked_words", JSON.stringify(bookmarkedObj));
-        displaybookmarkedWord();
-
-    }
-    else {
-        bookmark_icon.getElementsByTagName("i")[0].className = "bi bi-bookmark";
-        bookmarkedObj
-
-    }
-
-
-
-
-
-})
-function displaybookmarkedWord() {
-    let bookmarked_words = localStorage.getItem("bookmarked_words");
-    if (bookmarked_words == null) {
-        bookmarkedObj = [];
-    } else {
-        bookmarkedObj = JSON.parse(bookmarked_words);
-
-    }
-    let html = "";
-    bookmarkedObj.forEach(function (element, index) {
-        // console.log("entered recentWord loop  fnc")
-        html += `<li id=${index}><h4>${element}<button id=${index} type="button" class="btn-close text-reset"
-        aria-label="Close" onclick="delete_bookmarkWord(this.id)"></button></h4></li><br>`;
-        console.log(`index :${index} passed to delete function`)
-    });
-    let bookmarked_words_element = document.getElementById('bookmarked_words_element')
-    if (bookmarkedObj.length != 0) {
-
-        bookmarked_words_element.innerHTML = html;
-
-    }
-    else {
-        bookmarked_words_element.innerHTML = `<h5>No words bookmarked !!</h5>`
+    } catch (err) {
+        cardTitle.innerText = err.title || "Word Not Found";
+        wordMeaning.innerHTML = `<b><i>Error:</i></b> ${err.message || "No results found."}`;
+        wordExample.innerHTML = `<b><i>Note:</i></b> ${err.resolution || ""}`;
+        wordPronunciation.innerText = "";
+        audioBtn.style.display = "none";
+        bookmarkIcon.style.display = "none";
     }
 }
-function delete_bookmarkWord(index) {
-    let bookmarked_words = localStorage.getItem("bookmarked_words");
-    if (bookmarked_words == null) {
-        bookmarkedObj = [];
-    } else {
-        bookmarkedObj = JSON.parse(bookmarked_words);
 
-    }
-    console.log(`index :${index} received inside delete function`)
-    bookmarkedObj.splice(index, 1);
-    console.log(`index :${index} deleted`)
-    localStorage.setItem("bookmarked_words", JSON.stringify(bookmarkedObj));
-    displaybookmarkedWord();
+// Audio button
+audioBtn.addEventListener("click", () => {
+    audioBtn.querySelector("i").className = "bi bi-volume-down-fill";
+    wordAudio.load();
+    wordAudio.play();
+    wordAudio.onended = () => {
+        audioBtn.querySelector("i").className = "bi bi-volume-down";
+    };
+});
+
+// Save to recent
+function saveToRecent(word) {
+    const item = {
+        title: cardTitle.innerText,
+        meaning: wordMeaning.innerText.substring(0, 120) + "..."
+    };
+    let recent = JSON.parse(localStorage.getItem("recently_searched") || "[]");
+    recent.push(item);
+    if (recent.length > 5) recent.shift();
+    localStorage.setItem("recently_searched", JSON.stringify(recent));
+    displayRecentWords();
 }
 
+// Display recent
+function displayRecentWords() {
+    let recent = JSON.parse(localStorage.getItem("recently_searched") || "[]");
+    const container = document.getElementById("recently_searched");
+    if (recent.length === 0) {
+        recentTitle.style.display = "none";
+        container.innerHTML = "";
+        return;
+    }
+    recentTitle.style.display = "block";
+    container.innerHTML = recent.map(r => `
+        <div class="recent_word my-2 mx-2 card" style="width: 20rem;">
+            <div class="card-body">
+                <button id="recent_popover" type="button" class="btn" data-bs-toggle="popover"
+                    data-bs-trigger="hover focus" data-bs-placement="bottom"
+                    title="${r.title}" data-bs-content="${r.meaning}">
+                    <h5 class="card-title">${r.title}</h5>
+                </button>
+            </div>
+        </div>
+    `).join('');
 
-/// popovser section for arecent searches 
-var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
-var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
-    return new bootstrap.Popover(popoverTriggerEl)
-})
+    // Activate popovers
+    new bootstrap.Popover(document.querySelector('[data-bs-toggle="popover"]'));
+}
 
-/**
- * Back to top button
- */
+// Bookmarks
+bookmarkIcon.addEventListener("click", () => {
+    let word = cardTitle.innerText;
+    let bookmarks = JSON.parse(localStorage.getItem("bookmarked_words") || "[]");
+    if (!bookmarks.includes(word)) {
+        bookmarks.push(word);
+        bookmarkIcon.querySelector("i").className = "bi bi-bookmark-check-fill";
+    } else {
+        bookmarks = bookmarks.filter(w => w !== word);
+        bookmarkIcon.querySelector("i").className = "bi bi-bookmark";
+    }
+    localStorage.setItem("bookmarked_words", JSON.stringify(bookmarks));
+    displayBookmarks();
+});
 
-//Get the button
-let mybutton = document.getElementById("btn-back-to-top");
+function displayBookmarks() {
+    let bookmarks = JSON.parse(localStorage.getItem("bookmarked_words") || "[]");
+    const container = document.getElementById("bookmarked_words_element");
+    if (bookmarks.length === 0) {
+        container.innerHTML = "<h5>No words bookmarked!</h5>";
+        return;
+    }
+    container.innerHTML = bookmarks.map((word, index) => `
+        <li id="${index}">
+            <h4>${word}
+                <button id="${index}" type="button" class="btn-close text-reset"
+                    aria-label="Close" onclick="deleteBookmark(${index})">
+                </button>
+            </h4>
+        </li>
+        <br>
+    `).join('');
+}
 
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function () {
-    scrollFunction();
+function deleteBookmark(index) {
+    let bookmarks = JSON.parse(localStorage.getItem("bookmarked_words") || "[]");
+    bookmarks.splice(index, 1);
+    localStorage.setItem("bookmarked_words", JSON.stringify(bookmarks));
+    displayBookmarks();
+}
+
+// Back to top button
+let backToTopBtn = document.getElementById("btn-back-to-top");
+window.onscroll = () => {
+    backToTopBtn.style.display = (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) ? "block" : "none";
 };
-
-function scrollFunction() {
-    if (
-        document.body.scrollTop > 20 ||
-        document.documentElement.scrollTop > 20
-    ) {
-        mybutton.style.display = "block";
-    } else {
-        mybutton.style.display = "none";
-    }
-}
-// When the user clicks on the button, scroll to the top of the document
-mybutton.addEventListener("click", backToTop);
-
-function backToTop() {
+backToTopBtn.addEventListener("click", () => {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
-}
+});
